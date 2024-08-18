fcf0430de67ee6a638df4b1fad243268e8c47504
Arun <chan0392@e.ntu.edu.sg>
2024-08-18 03:08:08 -0700

    Add .readthedocs.yaml file to build

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,34 +2,33 @@
 
 # -- Project information
 
-project = 'MCTB2'
-copyright = '2018, Daniel M. Ingram'
-author = 'Daniel M. Ingram'
+project = "MCTB2"
+copyright = "2018, Daniel M. Ingram"
+author = "Daniel M. Ingram"
 
-release = '0.1'
-version = '0.1.0'
+release = "0.1"
+version = "0.1.0"
 
 # -- General configuration
 
 extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
+    "sphinx.ext.duration",
+    "sphinx.ext.doctest",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.intersphinx",
 ]
 
 intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+    "python": ("https://docs.python.org/3/", None),
+    "sphinx": ("https://www.sphinx-doc.org/en/master/", None),
 }
-intersphinx_disabled_domains = ['std']
+intersphinx_disabled_domains = ["std"]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # -- Options for HTML output
-
-html_theme = 'press'
+html_theme = "press"
 
 # -- Options for EPUB output
-epub_show_urls = 'footnote'
+epub_show_urls = "footnote"
